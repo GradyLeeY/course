b1320655258cdec49714f0d636d38fd2d177b194
ly <1016627132@qq.com>
2020-05-16 16:12:58 +0800

    新增gateway模块并将gatew注册到注册中心

---
+++ gateway/src/main/java/com/grady/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.grady.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+    public static void main(String[] args) {
+
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("gateway地址: http://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
