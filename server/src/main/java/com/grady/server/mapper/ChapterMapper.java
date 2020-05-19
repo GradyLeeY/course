4bcd7c0d551c7c684bc873de2b1427b79a0027ab
Grady <1016627132@qq.com>
2020-05-19 22:42:01 +0800

    '生成了chapter的xml并解决了id在sql中报错的问题(在language中将sql去掉，这是mybatis1.3的弊病)，暂时没有错误'

---
+++ server/src/main/java/com/grady/server/mapper/ChapterMapper.java
@@ -0,0 +1,30 @@
+package com.grady.server.mapper;
+
+import com.grady.server.domain.Chapter;
+import com.grady.server.domain.ChapterExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ChapterMapper {
+    long countByExample(ChapterExample example);
+
+    int deleteByExample(ChapterExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Chapter record);
+
+    int insertSelective(Chapter record);
+
+    List<Chapter> selectByExample(ChapterExample example);
+
+    Chapter selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Chapter record, @Param("example") ChapterExample example);
+
+    int updateByExample(@Param("record") Chapter record, @Param("example") ChapterExample example);
+
+    int updateByPrimaryKeySelective(Chapter record);
+
+    int updateByPrimaryKey(Chapter record);
+}
