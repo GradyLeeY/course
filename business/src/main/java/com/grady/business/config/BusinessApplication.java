21c0fa6d0fdd34813030b91dff9b5ee2376aad68
Grady <1016627132@qq.com>
2020-05-24 13:58:59 +0800

    新增business模块，增加初始代码

---
+++ business/src/main/java/com/grady/business/config/BusinessApplication.java
@@ -0,0 +1,26 @@
+package com.grady.business.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+/**
+ * @Author Grady
+ * @Date 2020/5/24 13:49
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class BusinessApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+    public static void main(String[] args) {
+
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("business启动成功！！");
+        LOG.info("business地址: http://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
