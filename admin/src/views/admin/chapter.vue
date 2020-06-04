36a547dbca301c23dd9d4d7295b1267326e6bf81
Grady <1016627132@qq.com>
2020-06-04 22:54:23 +0800

    集成sweetalert，确认时弹出确认框

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -160,11 +160,28 @@
       },
       del(id){
         let _this = this;
-        _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/'+id).then((response)=>{
-          let res = response.data;
-          if (res.success){
-            console.log('删除成功');
-            _this.list(1);
+
+        Swal.fire({
+          title: '确认删除id为'+id+'这条数据嘛',
+          text: '删除后不可恢复，确认删除?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: '确认!'
+        }).then((result)=>{
+          if (result.value){
+            _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/'+id).then((response)=>{
+              let res = response.data;
+              if (res.success){
+                _this.list(1);
+                Swal.fire(
+                  '删除成功',
+                  '该数据已删除',
+                  'success'
+                );
+              }
+            })
           }
         })
       },
