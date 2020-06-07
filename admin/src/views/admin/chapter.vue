a1f4071f28a1be883876faabcc7a95d0e9d4545a
Grady <1016627132@qq.com>
2020-06-07 12:13:30 +0800

    制作了comfirm组件

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -146,7 +146,7 @@
           if (res.success){
             $('#form-modal').modal('hide');
             _this.list(1);
-            toast.success("保存成功");
+            Toast.success("保存成功");
           }
         })
       },
@@ -163,26 +163,19 @@
       },
       del(id){
         let _this = this;
-        Swal.fire({
-          title: '确认删除id为'+id+'这条数据嘛',
-          text: '删除后不可恢复，确认删除?',
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: '确认!'
-        }).then((result)=>{
-          if (result.value){
-            _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/'+id).then((response)=>{
-              Loading.show();
-              let res = response.data;
-              if (res.success){
-                _this.list(1);
-                toast.success("删除成功");
-              }
-            })
-          }
-        })
+        Confirm.show("是否确认删除"+id+"这个大章",function () {
+          Loading.show();
+          _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/'+id).then((response)=>{
+            Loading.hide();
+            console.log("删除大章列表结果",response);
+            let res = response.data;
+            if(res.success){
+              _this.list(1);
+              Toast.success("删除成功");
+            }
+          })
+        });
+
       },
       list(page){
         let _this = this;
