4f592e58377aca91cd3ee7433a5d7baa7849c3b3
Grady <1016627132@qq.com>
2020-07-18 01:06:37 +0800

    '讲师头像显示优化'

--- file/src/main/java/com/course/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/course/file/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.course.file.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,9 +12,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Value("${file.path}")
+    private String FILE_PATH;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/f/**").addResourceLocations("file:D:/file/grady/course/");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:" + FILE_PATH);
         //http://127.0.0.1:9003/file/f/teacher/6XfOcgLk-Grady.jpg
     }
 }
--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -4,6 +4,7 @@ import com.grady.server.dto.ResponseDto;
 import com.grady.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +26,12 @@ public class UploadController {
     private static final Logger LOG = LoggerFactory.getLogger(UploadController.class);
     public static final String BUSINESS_NAME = "文件上传";
 
+    @Value("${file.domain}")
+    private String FILE_DOMAIN;
+
+    @Value("${file.path}")
+    private String FILE_PATH;
+
     @PostMapping("/upload")
     public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
         LOG.info("上传文件开始：{}", file);
@@ -35,12 +42,12 @@ public class UploadController {
         String fileName = file.getOriginalFilename();
         String key = UuidUtil.getShortUuid();
 
-        String fullPath = "D:/file/grady/course/teacher/" + key + "-" + fileName;
+        String fullPath = FILE_PATH + "teacher/" + key + "-" + fileName;
         File dest = new File(fullPath);
         file.transferTo(dest);
         LOG.info(dest.getAbsolutePath());
         ResponseDto responseDto = new ResponseDto();
-        responseDto.setContent("http://127.0.0.1:9000/file/f/teacher/" + key + "-" + fileName);
+        responseDto.setContent(FILE_DOMAIN + "f/teacher/" + key + "-" + fileName);
         return responseDto;
     }
 }
