27d461fd9ebdadcdbbfdb7915f2ceb02fc97aeab
Grady <1016627132@qq.com>
2020-07-17 13:02:58 +0800

    '文件上传功能:springboot+vue实现文件上传功能'

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -85,12 +85,7 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
-
-                  <div v-show="teacher.image" class="row">
-                    <div class="col-md-4">
-                      <img v-bind:src="teacher.image" class="img-responsive">
-                    </div>
-                  </div>
+                  <input type="file" v-on:change="uploadImage()" id="file-upload-input" />
                 </div>
               </div>
               <div class="form-group">
@@ -221,6 +216,15 @@
           })
         });
       },
+      uploadImage(){
+        let _this = this;
+        let formData = new window.FormData();
+        formData.append('file',document.querySelector("#file-upload-input").files[0]);
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload',formData).then((response)=>{
+          let resp = response.data;
+        })
+
+      }
     }
   }
 </script>
---
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -0,0 +1,45 @@
+package com.course.file.controller.admin;
+
+import com.grady.server.dto.ResponseDto;
+import com.grady.server.util.UuidUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @Author Grady
+ * @Date 2020/7/17 12:51
+ * @Version 1.0
+ */
+@RequestMapping("/admin")
+@RestController
+public class UploadController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UploadController.class);
+    public static final String BUSINESS_NAME = "文件上传";
+    public static final String filePreffix = "D:/Grady/Git/course/doc/image/";
+
+    @PostMapping("/upload")
+    public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
+        LOG.info("上传文件开始:{}",file);
+        LOG.info(file.getOriginalFilename());
+        LOG.info(String.valueOf(file.getSize()));
+
+        //保存到本地
+        String fileName = file.getOriginalFilename();
+        String key = UuidUtil.getShortUuid();
+        String fullPath =  filePreffix+key+"-"+fileName;
+        File dest = new File(fullPath);
+        file.transferTo(dest);
+        LOG.info(dest.getAbsolutePath());
+        ResponseDto responseDto = new ResponseDto();
+        return responseDto;
+    }
+}
