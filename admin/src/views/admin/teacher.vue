37938c5bbe9ee3ac20dc931dfaa9800cd93002dc
Grady <1016627132@qq.com>
2020-07-18 09:51:41 +0800

    文件上传功能开发：使用单独的上传按钮

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -85,7 +85,11 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
-                  <input type="file" v-on:change="uploadImage()" id="file-upload-input" />
+                  <button type="button" v-on:click="selectImage()" class="btn btn-white btn-default btn-round">
+                    <i class="ace-icon fa fa-upload"></i>
+                    上传头像
+                  </button>
+                  <input class="hidden" type="file" v-on:change="uploadImage()" id="file-upload-input" />
                   <div class="row" v-show="teacher.image">
                     <div class="col-md-4">
                       <img v-bind:src="teacher.image" class="img-responsive">
@@ -231,6 +235,9 @@
           _this.teacher.image = img;
         })
 
+      },
+      selectImage(){
+        $('#file-upload-input').trigger('click');
       }
     }
   }
