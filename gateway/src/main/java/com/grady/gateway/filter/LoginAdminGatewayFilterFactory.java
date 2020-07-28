79c043dacd18cfdadf335247849d74c66252c46d
Grady <1016627132@qq.com>
2020-07-29 00:29:20 +0800

    用户管理与登录：在gateway中增加登录拦截器

---
+++ gateway/src/main/java/com/grady/gateway/filter/LoginAdminGatewayFilterFactory.java
@@ -0,0 +1,22 @@
+package com.grady.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author Grady
+ * @Date 2020/7/29 0:27
+ * @Version 1.0
+ */
+public class LoginAdminGatewayFilterFactory  extends AbstractGatewayFilterFactory<Object> {
+
+    @Resource
+    LoginAdminGatewayFilter loginAdminGatewayFilter;
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return loginAdminGatewayFilter;
+    }
+}
