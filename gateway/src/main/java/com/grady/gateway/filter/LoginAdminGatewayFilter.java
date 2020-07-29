c2bc1bd9f78b842fc73d9842c9fb916aa4df5593
Grady <1016627132@qq.com>
2020-07-29 22:18:25 +0800

    用户管理与登录：gateway实现控台登录拦截功能

--- gateway/src/main/java/com/grady/gateway/filter/LoginAdminGatewayFilter.java
+++ gateway/src/main/java/com/grady/gateway/filter/LoginAdminGatewayFilter.java
@@ -5,9 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
 
 /**
  * @Author Grady
@@ -19,12 +23,39 @@ public class LoginAdminGatewayFilter implements GatewayFilter, Ordered {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoginAdminGatewayFilter.class);
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        //获取header的token参数
+        String path = exchange.getRequest().getURI().getPath();
+
+        //请求地址中不包含admin，不是控台请求，不需要拦截
+        if (!path.contains("/admin/")){
+            return chain.filter(exchange);
+        }
+
+        if (path.contains("/system/admin/user/login") || path.contains("/system/admin/user/logout") || path.contains("/system/admin/kaptcha")){
+            LOG.info("不需要控台验证:{}",path);
+            return chain.filter(exchange);
+        }
+
         String token = exchange.getRequest().getHeaders().getFirst("token");
-        LOG.info("登录拦截开始，token：{}", token);
-        return chain.filter(exchange);
+        LOG.info("后台登录验证开始,token:{}",token);
+        if (token ==null || token.isEmpty()){
+            LOG.info("没有token，请求被拦截");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        Object o = redisTemplate.opsForValue().get("token");
+        if (o == null){
+            LOG.warn("token无效，请求被拦截");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }else {
+            LOG.info("已登录{}",o);
+            return chain.filter(exchange);
+        }
     }
 
     @Override
--- gateway/src/main/java/com/grady/gateway/filter/LoginAdminGatewayFilterFactory.java
+++ gateway/src/main/java/com/grady/gateway/filter/LoginAdminGatewayFilterFactory.java
@@ -2,6 +2,7 @@ package com.grady.gateway.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
@@ -10,6 +11,7 @@ import javax.annotation.Resource;
  * @Date 2020/7/29 0:27
  * @Version 1.0
  */
+@Component
 public class LoginAdminGatewayFilterFactory  extends AbstractGatewayFilterFactory<Object> {
 
     @Resource
